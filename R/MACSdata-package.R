a8dea8606e6add41a87b40f44168008f07c81cf9
qhu75 <qhu75@gatech.edu>
2020-12-08 10:34:00 -0500

    init

---
+++ R/MACSdata-package.R
@@ -0,0 +1,7 @@
+#' MACSdata
+#'
+#' This is an ExperimentHub Package for files that used in MACS unit
+#' test. Details can be found at: https://macs3-project.github.io/macs/.
+#' @docType package
+#' @name MACSdata
+NULL
---
+++ inst/scripts/make-metadata.R
@@ -0,0 +1,31 @@
+
+
+testcmd <- readLines("https://raw.githubusercontent.com/macs3-project/MACS/master/test/cmdlinetest")
+f1 <- sub(".*=", "", grep("^CHIP", testcmd, value = TRUE))
+f2 <- sub(".*=", "", grep("^CTRL", testcmd, value = TRUE))
+fs <- c(f1, f2)
+type <- rep("BED", length(fs))
+type[grep(".bam", fs)] <- "BAM"
+cbase <- rep(FALSE, length(fs))
+cbase[type == "BAM"] <- TRUE
+
+meta_data <- data.frame(
+    Title = rep("MACS test datasets", 9),
+    Description = sub(".bed.gz|.bedpe.gz|.bam", "", fs),
+    BiocVersion = "3.12",
+    Genome = NA,
+    SourceType= type,
+    SourceUrl = "https://github.com/macs3-project/MACS/tree/master/test",
+    SourceVersion = "v1",
+    Species = "Home sapiens",
+    TaxonomyId="9606",
+    Coordinate_1_based = cbase,
+    DataProvider = "MACS",
+    Maintainer = "Qiang Hu <qiang.hu@roswellpark.org>",
+    RDataClass="character",
+    DispatchClass="FilePath",
+    RDataPath = file.path("MACSdata", fs),
+    stringsAsFactors = FALSE
+)
+
+write.csv(meta_data, file = "metadata.csv", row.names = FALSE)
